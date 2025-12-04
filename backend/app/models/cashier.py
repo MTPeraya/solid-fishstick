5b70e7de23e2ae739ca5c75ecb56e78fb485d520
tanoy <128113156+tanoy3800@users.noreply.github.com>
2025-12-05 04:42:43 +0700

    add pos and product and transaction routes

--- backend/app/models/cashier.py
+++ backend/app/models/cashier.py
@@ -2,5 +2,4 @@ from sqlmodel import SQLModel, Field
 
 
 class Cashier(SQLModel, table=True):
-    employee_uid: str = Field(primary_key=True, foreign_key="user.uid")
-    position: str
+    employee_id: str = Field(primary_key=True, foreign_key="user.uid")
