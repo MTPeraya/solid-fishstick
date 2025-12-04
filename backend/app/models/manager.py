de20c10f4495e0ad6310131ec0598c2bf9123a61
tanoy <128113156+tanoy3800@users.noreply.github.com>
2025-12-05 04:42:43 +0700

    add pos and product and transaction routes

--- backend/app/models/manager.py
+++ backend/app/models/manager.py
@@ -2,4 +2,4 @@ from sqlmodel import SQLModel, Field
 
 
 class Manager(SQLModel, table=True):
-    admin_uid: str = Field(primary_key=True, foreign_key="user.uid")
+    admin_id: str = Field(primary_key=True, foreign_key="user.uid")
