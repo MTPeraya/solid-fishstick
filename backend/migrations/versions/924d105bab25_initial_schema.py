8554f91fa771578ac48748690d7cfa274ed32054
MTPeraya <m.perayalsc@gmail.com>
2025-12-11 23:18:38 +0700

    reset and clean migration system

---
+++ backend/migrations/versions/924d105bab25_initial_schema.py
@@ -0,0 +1,23 @@
+"""initial schema
+
+Revision ID: 924d105bab25
+Revises: None
+Create Date: 2025-12-11 16:09:44.545936
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision = '924d105bab25'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
